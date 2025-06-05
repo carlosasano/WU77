204fb8104b36dcab98f34ceb81041d2047242ca4
carlosasano <caduasano@gmail.com>
2025-06-05 20:24:28 -0300

    Update testedeidentacao.py

--- testedeidentacao.py
+++ testedeidentacao.py
@@ -1,9 +1,10 @@
 #identação
 if 5>2:
     print("eu vou ali jaja")
-
+#essa e uma estrutura identada
 if 5>3:
     print("aqui não e o lugar correto")
-
+#essa e uma estrutura identada
 if 10>11:
     print("estou com fome")
+
